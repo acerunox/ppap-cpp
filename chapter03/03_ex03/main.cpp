04f178e33929151dbf3d44cfcd52d03fe57f40a1
Szymon Mokrzycki <7107537+acerunox@users.noreply.github.com>
2020-04-23 18:56:49 +0200

    Add 03_ex03

---
+++ chapter03/03_ex03/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+int main()
+{
+    int double = 0;
+    std::string int = "Is this an int or string?";
+    std::string double = "stringstring";
+    double 2names = 2;
+    char string = "string";
+    int age ? = "18";
+}
