67487a601c25aaf508f035c58ecc95f743270ff6
Szymon Mokrzycki <7107537+acerunox@users.noreply.github.com>
2020-06-03 22:06:29 +0200

    Add 08_ex03

---
+++ chapter08/08_ex03/main.cpp
@@ -0,0 +1,34 @@
+#include "../../lib/utilities.h"
+#include <iostream>
+#include <vector>
+
+void Print(const std::vector<int> &numbers, const std::string &label)
+{
+    std::cout << label << ": ";
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        std::cout << numbers[i];
+        if (i != numbers.size() - 1) std::cout << ", ";
+    }
+    std::cout << '\n';
+}
+
+void Fibonacci(const int &x, const int &y, std::vector<int> &vector, const int &amount)
+{
+    if (x == 0 && y == 0) ThrowError("[ERROR] At least one number has to be greater than 0.");
+    if (amount <= 0) ThrowError("[ERROR] Ask for at least one number.");
+
+    vector.push_back(x);
+    if (amount == 1) return;
+    vector.push_back(y);
+
+    for (int i = 2; i < amount; ++i) vector.push_back(vector[i - 2] + vector[i - 1]);
+}
+
+int main()
+{
+    std::vector<int> sequence;
+
+    Fibonacci(1, 2, sequence, 10);
+    Print(sequence, "Sequence");
+}
