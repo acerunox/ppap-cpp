ec9ba191a8f3946f5695135e33ed13951a0dd7d7
Szymon Mokrzycki <7107537+acerunox@users.noreply.github.com>
2020-06-03 21:12:21 +0200

    Add 08_ex02

---
+++ chapter08/08_ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+void Print(const std::vector<int> &numbers, const std::string &label)
+{
+    std::cout << label << ": ";
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        std::cout << numbers[i];
+        if (i != numbers.size() - 1) std::cout << ", ";
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::vector<int> numbers { 10, 7, 18, 13, 33, 20, 11 };
+
+    Print(numbers, "Entries");
+}
