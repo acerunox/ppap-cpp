ca2f81f8e27f1be572c49701347e330e8407f566
Szymon Mokrzycki <7107537+acerunox@users.noreply.github.com>
2020-06-08 18:54:44 +0200

    Add 08_ex10

---
+++ chapter08/08_ex10/main.cpp
@@ -0,0 +1,22 @@
+#include "../../lib/utilities.h"
+#include <iostream>
+#include <limits.h>
+#include <vector>
+
+int MaxV(const std::vector<int> &vector)
+{
+    if (vector.size() == 0) ThrowError("[ERROR] Vector cannot be empty.");
+    
+    int max = INT_MIN;
+    for (const int &value : vector)
+    {
+        if (value > max) max = value;
+    }
+    return max;
+}
+
+int main()
+{
+    std::vector<int> numbers{ 2342, 432, 12, 5433, 99142, 32, 1, 349533 };
+    std::cout << MaxV(numbers) << '\n';
+}
