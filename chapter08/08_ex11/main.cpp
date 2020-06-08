8813998632a671570d11f647738bbc6ec415e5b0
Szymon Mokrzycki <7107537+acerunox@users.noreply.github.com>
2020-06-08 19:46:09 +0200

    Add 08_ex11

---
+++ chapter08/08_ex11/main.cpp
@@ -0,0 +1,57 @@
+#include "../../lib/utilities.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+struct VectorProperties
+{
+    public:
+        VectorProperties(const std::vector<int> &vector);
+        int min;
+        int max;
+        double mean;
+        double median;
+
+    private:
+        void ComputeProperties(std::vector<int> vector);
+};
+
+VectorProperties::VectorProperties(const std::vector<int> &vector)
+{
+    ComputeProperties(vector);
+}
+
+void VectorProperties::ComputeProperties(std::vector<int> vector)
+{
+    std::sort(vector.begin(), vector.end());
+    min = vector.front();
+    max = vector.back();
+
+    double sum = 0.0;
+    for (const int &number : vector) sum += number;
+    mean = sum / vector.size();
+
+    if (vector.size() % 2 == 0)
+    {
+        median = (vector[vector.size() / 2 - 1] + vector[vector.size() / 2]) / 2.0;
+    }
+    else median = vector[vector.size() / 2];
+}
+
+int main()
+{
+    std::vector<int> numbers{ 53, 9843, 231, 4352, 33, 94, 21234, 2 };
+    VectorProperties numbersProperties { numbers };
+
+    for (int i = 0; i < numbers.size(); ++i)
+    {
+        std::cout << numbers[i];
+        if (i != numbers.size() - 1) std::cout << ", ";
+    }
+
+    std::cout << "\n\nProperties:\n"
+        << "Min: " << numbersProperties.min << '\n'
+        << "Max: " << numbersProperties.max << '\n'
+        << "Mean: " << numbersProperties.mean << '\n'
+        << "Median: " << numbersProperties.median << '\n';
+}
